10ccbbe13357ffdce6465ea916eb5a0abfb87db6
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-18 02:16:02 +0800

    feat: project page

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -1,4 +1,6 @@
 import { getProject } from '@/sanity/sanity.utils';
+import { PortableText } from '@portabletext/react';
+import Image from 'next/image';
 
 type Props = {
   params: { project: string };
@@ -9,7 +11,38 @@ export default async function Project({ params }: Props) {
 
   const project = await getProject(slug);
 
-  console.log(project);
+  return (
+    <div className='max-w-3xl mx-auto py-20'>
+      <header className='flex justify-between items-center'>
+        <h1 className='h-16 text-5xl font-extrabold drop-shadow-md bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent'>
+          {project?.name}
+        </h1>
+        <a
+          href={project?.url}
+          title='View Project'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='bg-gray-100 rounded-lg text-gray-500 font-bold py-3 px-4 whitespace-nowrap hover:bg-teal-500 hover:text-teal-100 transition'
+        >
+          View Project
+        </a>
+      </header>
 
-  return <div>My project goes here</div>;
+      <section>
+        <article className='text-lg font-semibold ml-1 text-gray-700 mt-5'>
+          <PortableText value={project?.content} />
+        </article>
+      </section>
+
+      <section>
+        <Image
+          src={project?.image}
+          alt={project?.name}
+          width={1920}
+          height={1080}
+          className='mt-10 border-2 drop-shadow-lg border-gray-700 object-cover rounded-xl hover:scale-105 transition ease-in-out'
+        />
+      </section>
+    </div>
+  );
 }
