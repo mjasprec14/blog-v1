f36d32e69c8bdf5cb5a9c658225e1022745a67af
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-18 03:30:28 +0800

    feat: getPage and getPages

--- sanity/sanity.utils.ts
+++ sanity/sanity.utils.ts
@@ -1,5 +1,5 @@
 import { createClient, groq } from 'next-sanity';
-import { Project } from '@/types';
+import { Project, Page } from '@/types';
 import config from './config/client-config';
 
 export async function getProjects(): Promise<Project[]> {
@@ -11,6 +11,19 @@ export async function getProjects(): Promise<Project[]> {
 export async function getProject(slug: string): Promise<Project> {
   return await createClient(config).fetch(
     groq`*[_type=='project' && slug.current == $slug][0]{_id,_createdAt,name,"slug":slug.current,"image": image.asset->url, url, content}`,
+    { slug }
+  );
+}
+
+export async function getPages(): Promise<Page[]> {
+  return await createClient(config).fetch(
+    groq`*[_type=='page']{_id,_createdAt,title,"slug":slug.current}`
+  );
+}
+
+export async function getPage(slug: string): Promise<Page> {
+  return await createClient(config).fetch(
+    groq`*[_type=='page' && slug.current == $slug][0]{_id,_createdAt,title,"slug":slug.current, content}`,
     { slug }
   );
 }
