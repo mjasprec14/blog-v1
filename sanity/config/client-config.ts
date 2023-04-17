0fd81144fc88310a2b54eeeaea3a7c3fe67c01f7
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-18 01:58:23 +0800

    feat: client config

---
+++ sanity/config/client-config.ts
@@ -0,0 +1,7 @@
+const config = {
+  projectId: 'jswvt59x',
+  dataset: 'production',
+  apiVersion: '2023-04-16',
+};
+
+export default config;
--- sanity/sanity.utils.ts
+++ sanity/sanity.utils.ts
@@ -1,26 +1,15 @@
 import { createClient, groq } from 'next-sanity';
 import { Project } from '@/types';
+import config from './config/client-config';
 
 export async function getProjects(): Promise<Project[]> {
-  const client = createClient({
-    projectId: 'jswvt59x',
-    dataset: 'production',
-    apiVersion: '2023-04-16',
-  });
-
-  return await client.fetch(
+  return await createClient(config).fetch(
     groq`*[_type=='project']{_id,_createdAt,name,"slug":slug.current,"image": image.asset->url, url, content}`
   );
 }
 
 export async function getProject(slug: string): Promise<Project> {
-  const client = createClient({
-    projectId: 'jswvt59x',
-    dataset: 'production',
-    apiVersion: '2023-04-16',
-  });
-
-  return await client.fetch(
+  return await createClient(config).fetch(
     groq`*[_type=='project' && slug.current == $slug][0]{_id,_createdAt,name,"slug":slug.current,"image": image.asset->url, url, content}`,
     { slug }
   );
